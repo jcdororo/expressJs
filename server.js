908201730c71c430e001217b40bd26f057d03705
jc <wnwlcks123@gmail.com>
2023-10-09 15:47:32 +0900

    req.params

--- server.js
+++ server.js
@@ -2,9 +2,35 @@ const express = require('express');
 
 const PORT = 4000;
 
-const app =express();
+const Users = [
+  {
+    id: 0,
+    name: 'Jack'
+  },
+  {
+    id: 1,
+    name: 'Jennifer'
+  }
+]
+
+const app = express();
+
+app.get('/users', (req, res) => {
+  res.send(Users);
+})
+
 app.get('/', (req, res) => {
   res.send('Hello, World!');
+})
+
+app.get('/users/:userId', (req, res) => {
+  const userId = Number(req.params.userId);
+  const user = Users[userId];
+  if(user) {
+    res.json(user);
+  } else {
+    res.status(404);
+  }
 })
 
 app.listen(PORT, () => {
