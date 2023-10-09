9581813318981c819b75eafbde72442a571a3eb4
jc <wnwlcks123@gmail.com>
2023-10-09 16:25:02 +0900

    POST 요청하기_ express.json()

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const Users = [
 ]
 
 const app = express();
+app.use(express.json());
 
 app.use((req, res, next) => {
   const start = Date.now();
@@ -30,6 +31,24 @@ app.get('/users', (req, res) => {
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
+})
+
+app.post('/users', (req, res) => {
+
+  if(!req.body.name) {
+    return res.status(400).json({
+      error: "Missing user name"
+    })
+  }
+
+
+  // console.log('req.body.name: ' + req.body.name)
+  const newUser = {
+    name: req.body.name,
+    id: Users.length
+  }
+  Users.push(newUser);
+  res.json(newUser);
 })
 
 app.get('/users/:userId', (req, res) => {
