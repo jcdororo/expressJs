fc13cb9ee1880aa95b659f4122ada6b22c44d370
jc <wnwlcks123@gmail.com>
2023-10-10 13:25:59 +0900

    Update, Delete 구현

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -31,10 +31,42 @@ async function getProductById(req, res, next) {
   }
 }
 
+async function updateProduct(req, res, next) {
+  try {
+    let updatedProduct = await productModel.findByIdAndUpdate(
+      req.params.productId,
+      req.body,
+      { new: true }
+    )
+    if(updatedProduct) {
+      res.status(200).json(updatedProduct);
+    } else {
+      res.status(404).send();
+    }
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function deleteProduct(req, res, next) {
+  try {
+    let deletedProduct = await productModel.findByIdAndDelete(req.params.productId);
+    if(deletedProduct) {
+      res.status(200).json(deletedProduct);
+    } else {
+      res.status(404).send();
+    }
+  } catch (error) {
+    next(error);
+  }
+}
+
 
 
 module.exports = {
   createProduct,
   getProducts,
-  getProductById
+  getProductById,
+  updateProduct,
+  deleteProduct
 }
--- routes/products.router.js
+++ routes/products.router.js
@@ -5,5 +5,7 @@ const productsRouter = express.Router();
 productsRouter.post('/', productsController.createProduct);
 productsRouter.get('/', productsController.getProducts);
 productsRouter.get('/:productId', productsController.getProductById);
+productsRouter.put('/:productId', productsController.updateProduct);
+productsRouter.delete('/:productId', productsController.deleteProduct);
 
 module.exports = productsRouter;
