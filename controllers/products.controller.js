8e417e4dd5dab6a824492b0c91a114547f89be79
jc <wnwlcks123@gmail.com>
2023-10-10 13:16:23 +0900

    Read 구현

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -9,6 +9,32 @@ async function createProduct(req, res, next) {
   }
 }
 
+async function getProducts(req, res, next) {
+  try {
+    const allProducts = await productModel.find({});
+    res.status(200).json(allProducts);
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function getProductById(req, res, next) {
+  try {
+    const product = await productModel.findById(req.params.productId);
+    if(product) {
+      res.status(200).json(product);
+    } else {
+      res.status(404).send();
+    }
+  } catch (error) {
+    next(error);
+  }
+}
+
+
+
 module.exports = {
-  createProduct
+  createProduct,
+  getProducts,
+  getProductById
 }
--- routes/products.router.js
+++ routes/products.router.js
@@ -3,5 +3,7 @@ const productsController = require('../controllers/products.controller');
 const productsRouter = express.Router();
 
 productsRouter.post('/', productsController.createProduct);
+productsRouter.get('/', productsController.getProducts);
+productsRouter.get('/:productId', productsController.getProductById);
 
 module.exports = productsRouter;
